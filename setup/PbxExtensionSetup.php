cf061619287ee289a347ef027912b6b38094e90c
Nikolay Beketov <nbek@miko.ru>
2018-09-25 10:32:19 +0300

    Initial commit

---
+++ setup/PbxExtensionSetup.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 9 2018
+ *
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: nikolaybeketov
+ * Date: 12.09.2018
+ * Time: 12:36
+ */
+class PbxExtensionSetup {
+
+	/**
+	 * Создает структуру для хранения настроек модуля в своей модели
+	 * и заполняет настройки по-умолчанию если таблицы не было в системе
+	 * см (unInstallDB)
+	 *
+	 * Регистрирует модуль в PbxExtensionModules
+	 * Регистрирует модуль в PbxExtensionRelationship
+	 *
+	 * @return bool результат установки
+	 */
+	function installDB() {
+		$result = TRUE;
+		$this->db->begin();
+
+		//...код
+
+		if ( $result ) {
+			$this->db->commit();
+		} else {
+			$this->db->rollback();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Выполняет копирование необходимых файлов, в папки системы
+	 *
+	 * @return bool результат установки
+	 */
+	function installFiles() {
+		return TRUE;
+	}
+
+	/**
+	 * Удаляет запись о модуле из PbxExtensionModules
+	 * Удаляет запись о модуле из PbxExtensionRelationship
+	 * Удаляет свою модель
+	 *
+	 * @param  $keepSettings - оставляет таблицу с данными своей модели
+	 *
+	 * @return bool результат очистки
+	 */
+	function unInstallDB( $keepSettings = FALSE ) {
+		$result = TRUE;
+		$this->db->begin();
+
+		//...код
+
+		if ( $result ) {
+			$this->db->commit();
+		} else {
+			$this->db->rollback();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Выполняет удаление своих файлов с остановной процессов
+	 * при необходимости
+	 *
+	 * @return bool результат удаления
+	 */
+	function unInstallFiles() {
+		return TRUE;
+	}
+
+}
