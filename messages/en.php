6cbf76d16776f5c18ade05af25da33016fa3d626
Nikolay Beketov <nbek@miko.ru>
2018-09-25 18:35:51 +0300

    Перенес перевод модулей в собственные папки

---
+++ messages/en.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+
+$messages = [
+	"repModuleTemplate"       => "Module template - %repesent%",
+	"mo_ModuleModuleTemplate" => "Module template",
+];
---
+++ messages/ru.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+
+$messages = [
+	"repModuleTemplate"       => "Модуль шаблон - %repesent%",
+	"mo_ModuleModuleTemplate" => "Модуль шаблон",
+];
