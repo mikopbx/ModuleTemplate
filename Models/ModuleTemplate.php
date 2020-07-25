8d74f4391aea9a05cf83e8c391a6cfbb92d322e7
Nikolay Beketov <nbek@miko.ru>
2020-07-25 21:09:29 +0300

    Мелкие правки

--- Models/ModuleTemplate.php
+++ Models/ModuleTemplate.php
@@ -5,6 +5,7 @@
  * Proprietary and confidential
  * Written by Alexey Portnov, 2 2019
  */
+
 /*
  * https://docs.phalconphp.com/3.4/ru-ru/db-models-metadata
  *
@@ -17,82 +18,66 @@ use MikoPBX\Common\Models\Providers;
 use MikoPBX\Modules\Models\ModulesModelsBase;
 use Phalcon\Mvc\Model\Relation;
 
-class ModuleTemplate extends ModulesModelsBase {
+class ModuleTemplate extends ModulesModelsBase
+{
 
     /**
      * @Primary
      * @Identity
      * @Column(type="integer", nullable=false)
      */
-	public $id;
+    public $id;
 
-	/**
-	 * Text field example
-	 *
-	 * @Column(type="string", nullable=true)
-	 */
-	public $text_field;
+    /**
+     * Text field example
+     *
+     * @Column(type="string", nullable=true)
+     */
+    public $text_field;
 
-	/**
-	 * TextArea field example
-	 *
-	 * @Column(type="string", nullable=true)
-	 */
-	public $text_area_field;
+    /**
+     * TextArea field example
+     *
+     * @Column(type="string", nullable=true)
+     */
+    public $text_area_field;
 
-	/**
-	 * Password field example
-	 *
-	 * @Column(type="string", nullable=true)
-	 */
-	public $password_field;
+    /**
+     * Password field example
+     *
+     * @Column(type="string", nullable=true)
+     */
+    public $password_field;
 
-	/**
-	 * Integer field example
-	 *
-	 * @Column(type="integer", default="1", nullable=true)
-	 */
-	public $integer_field;
+    /**
+     * Integer field example
+     *
+     * @Column(type="integer", default="1", nullable=true)
+     */
+    public $integer_field;
 
-	/**
-	 * CheckBox
-	 *
-	 * @Column(type="integer", default="1", nullable=true)
-	 */
-	public $checkbox_field;
+    /**
+     * CheckBox
+     *
+     * @Column(type="integer", default="1", nullable=true)
+     */
+    public $checkbox_field;
 
-	/**
-	 * Toggle
-	 *
-	 * @Column(type="integer", default="1", nullable=true)
-	 */
-	public $toggle_field;
+    /**
+     * Toggle
+     *
+     * @Column(type="integer", default="1", nullable=true)
+     */
+    public $toggle_field;
 
-	/**
-	 * Dropdown menu
-	 *
-	 * @Column(type="string", nullable=true)
-	 */
-	public $dropdown_field;
+    /**
+     * Dropdown menu
+     *
+     * @Column(type="string", nullable=true)
+     */
+    public $dropdown_field;
 
-	public function initialize() :void{
-        $this->setSource('m_ModuleTemplate');
-		$this->hasOne(
-			'dropdown_field',
-			Providers::class,
-			'id',
-			[
-				'alias'      => 'Providers',
-				'foreignKey' => [
-					'allowNulls' => TRUE,
-					'action'     => Relation::NO_ACTION,
-				],
-			]
-		);
-        parent::initialize();
-	}
-
-	/**
+    /**
      * Returns dynamic relations between module models and common models
      * MikoPBX check it in ModelsBase after every call to keep data consistent
      *
@@ -100,28 +85,47 @@ class ModuleTemplate extends ModulesModelsBase {
      *
      * It is important to duplicate the relation alias on message field after Models\ word
      *
+     * @param $calledModelObject
+     *
+     * @return void
      */
-	public static function getDynamicRelations($calledClass) :array{
-		$result = [];
-		if ($calledClass === Providers::class){
-			$result =[
-				'belongsTo'=>[
-					'id',
-					ModuleTemplate::class,
-					'dropdown_field',
-					[
-						'alias'=>'ModuleTemplateProvider',
-						'foreignKey' => [
-							'allowNulls' => 0,
-							'message'    => 'Models\ModuleTemplateProvider',
-							'action'     => Relation::ACTION_RESTRICT // запретить удалять провайдера если есть ссылки в модуле
-						]
-					]
-				],
-			];
-		}
-		return $result;
-	}
+    public static function getDynamicRelations(&$calledModelObject): void
+    {
+        if (is_a($calledModelObject, Providers::class)) {
+            $calledModelObject->belongsTo(
+                'id',
+                ModuleTemplate::class,
+                'dropdown_field',
+                [
+                    'alias'      => 'ModuleTemplateProvider',
+                    'foreignKey' => [
+                        'allowNulls' => 0,
+                        'message'    => 'Models\ModuleTemplateProvider',
+                        'action'     => Relation::ACTION_RESTRICT
+                        // запретить удалять провайдера если есть ссылки в модуле
+                    ],
+                ]
+            );
+        }
+    }
+
+    public function initialize(): void
+    {
+        $this->setSource('m_ModuleTemplate');
+        $this->hasOne(
+            'dropdown_field',
+            Providers::class,
+            'id',
+            [
+                'alias'      => 'Providers',
+                'foreignKey' => [
+                    'allowNulls' => true,
+                    'action'     => Relation::NO_ACTION,
+                ],
+            ]
+        );
+        parent::initialize();
+    }
 
 
 }
