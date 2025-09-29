afde6bf81e5863fe210ec079be027c5d4e7e32c2
Weblate <noreply@weblate.org>
2025-09-29 08:59:23 +0300

    Added translation using Weblate (Hebrew)

---
+++ Messages/he.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'repModuleTemplate' => '',
+    'mo_ModuleModuleTemplate' => '',
+    'BreadcrumbModuleTemplate' => '',
+    'SubHeaderModuleTemplate' => '',
+    'module_template_AddNewRecord' => '',
+    'module_template_TextFieldLabel' => '',
+    'module_template_TextAreaFieldLabel' => '',
+    'module_template_PasswordFieldLabel' => '',
+    'module_template_IntegerFieldLabel' => '',
+    'module_template_CheckBoxFieldLabel' => '',
+    'module_template_ToggleFieldLabel' => '',
+    'module_template_DropDownFieldLabel' => '',
+    'module_template_ValidateValueIsEmpty' => '',
+    'module_template_AdditionalMenuItem' => '',
+    'module_template_AdditionalTabContent' => '',
+    'module_template_AdditionalSubMenuItem' => '',
+    'module_template_ChangeRecord' => '',
+    'BreadcrumbAdditionalPage' => '',
+];
